a2b2bc9a97af9e12c86e3f0b2e37af6fadb4f306
Kortexik <karab471@gmail.com>
2025-04-22 08:03:24 +0200

    Fixed upload time format and cleaned the hard coded urls

--- backend/src/middleware/CORSmiddleware.go
+++ backend/src/middleware/CORSmiddleware.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://4.234.181.167:5173") // "http://4.234.181.167:5173"
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") 
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-CSRF-Token, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
