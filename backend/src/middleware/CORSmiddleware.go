0d239be4a4ed1fd0170a759391057f68b02c95a9
Kortexik <karab471@gmail.com>
2025-04-22 12:13:30 +0200

    fixed cors middleware

--- backend/src/middleware/CORSmiddleware.go
+++ backend/src/middleware/CORSmiddleware.go
@@ -1,11 +1,22 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://4.234.181.167:5173") // "http://4.234.181.167:5173"
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") 
+		origin := c.Request.Header.Get("Origin")
+
+		allowedOrigins := map[string]bool{
+			"http://4.234.181.167:5173": true,
+			"http://localhost:5173":     true,
+		}
+
+		if allowedOrigins[origin] {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-CSRF-Token, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
